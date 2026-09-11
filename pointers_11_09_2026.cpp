f4175c28bc506541b9b878c364a86ed5ae4464c6
banothanusha-78 <banothanusha900@gmail.com>
2026-09-11 08:12:37 +0000

    Add pointer practice code

--- pointers_11_09_2026.cpp
+++ pointers_11_09_2026.cpp
@@ -1,2 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x=25;
+    int* p=&x;
+    cout<<"value of x"<<*(&x)<<endl;
+    cout<<"Address of x:"<<(*p)<<endl;
+    cout<<"Value stroed in p:"<<*(&x)<<endl;
+    cout<<"Value pointed by p:"<<(&x)<<endl;
+
+    
+}
